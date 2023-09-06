51afbb1d24622ec11402db7a91845f8ed9ebd02a
E. Gökçen Solmaz <gokcensolmaz99@gmail.com>
2023-09-06 21:48:31 +0300

    deploy

---
+++ build/precache-manifest.b9a09c7abd1c1906969472d053ec66b7.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e65810f78da1f738d83",
+    "url": "/static/css/main.47a96ad4.chunk.css"
+  },
+  {
+    "revision": "2e65810f78da1f738d83",
+    "url": "/static/js/main.2e65810f.chunk.js"
+  },
+  {
+    "revision": "0d6e5c59ccacea66726b",
+    "url": "/static/js/1.0d6e5c59.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d827fd7095587fad48ecc82ac81d0207",
+    "url": "/static/media/tenorsans.d827fd70.ttf"
+  },
+  {
+    "revision": "41d92fd78c10238a92a7d24a74968f36",
+    "url": "/static/media/teslaFont.41d92fd7.ttf"
+  },
+  {
+    "revision": "52a37115b1d8d5d6ae0b0e373e692c9d",
+    "url": "/static/media/montserrat.52a37115.ttf"
+  },
+  {
+    "revision": "1a2bc98c9777294b47a55db20a21ca5b",
+    "url": "/static/media/porscheFont.1a2bc98c.ttf"
+  },
+  {
+    "revision": "a97dc52b4b2059e5f04907cb7492af0b",
+    "url": "/static/media/jaguarFont.a97dc52b.ttf"
+  },
+  {
+    "revision": "ec32613913a5173bcb3d13305adae6ff",
+    "url": "/static/media/maseratiFont.ec326139.otf"
+  },
+  {
+    "revision": "f9170535296ea144483f63eac8031ce0",
+    "url": "/static/media/marble_bg.f9170535.svg"
+  },
+  {
+    "revision": "e1b07e2214ab1e87c17905d1469925a0",
+    "url": "/static/media/OtoBee_white_logo.e1b07e22.svg"
+  },
+  {
+    "revision": "a5968ebbb2ad336b11c68cbe885a5cc9",
+    "url": "/static/media/tesla_white.a5968ebb.svg"
+  },
+  {
+    "revision": "cb36b2c249b8be395ff4789c5d4b4826",
+    "url": "/static/media/tesla_grey.cb36b2c2.svg"
+  },
+  {
+    "revision": "446bc294833e0e35f09dd5526268c73e",
+    "url": "/static/media/tesla_red.446bc294.svg"
+  },
+  {
+    "revision": "2217ed9233ff09098e6a053179bec96e",
+    "url": "/static/media/tesla_blue.2217ed92.svg"
+  },
+  {
+    "revision": "39976b17469a90eed14b3bd7bf9ed3d5",
+    "url": "/static/media/porsche_white.39976b17.svg"
+  },
+  {
+    "revision": "3519f71a3a3f40e137ba99883cc8203c",
+    "url": "/static/media/porsche_grey.3519f71a.svg"
+  },
+  {
+    "revision": "7cf8aa32640d5314ec8aa1f98dddc36f",
+    "url": "/static/media/porsche_red.7cf8aa32.svg"
+  },
+  {
+    "revision": "319012ddf3207528c29979949951df30",
+    "url": "/static/media/porsche_blue.319012dd.svg"
+  },
+  {
+    "revision": "5f6ccb8c2095d1825a6d2de1620a0ea0",
+    "url": "/static/media/maserati_white.5f6ccb8c.svg"
+  },
+  {
+    "revision": "f4e52bfd7e465af0e3790c06f2a14c43",
+    "url": "/static/media/maserati_grey.f4e52bfd.svg"
+  },
+  {
+    "revision": "b064885785cccf20656cacd7f8a79f14",
+    "url": "/static/media/maserati_red.b0648857.svg"
+  },
+  {
+    "revision": "fb790a8644725d029e528c4b76e26fa2",
+    "url": "/static/media/maserati_blue.fb790a86.svg"
+  },
+  {
+    "revision": "d18b4be300617c64c41454386ecf4715",
+    "url": "/static/media/jaguar_white.d18b4be3.svg"
+  },
+  {
+    "revision": "6a488cce945ee246ad19e2aa53c603af",
+    "url": "/static/media/jaguar_grey.6a488cce.svg"
+  },
+  {
+    "revision": "9fbc09453bb75758a11197264db59645",
+    "url": "/static/media/jaguar_red.9fbc0945.svg"
+  },
+  {
+    "revision": "4f360a330856fcf9e449842bd7a867aa",
+    "url": "/static/media/jaguar_blue.4f360a33.svg"
+  },
+  {
+    "revision": "269234e2fd66f4d61e48975c3dd40b3d",
+    "url": "/static/media/turkey_map.269234e2.svg"
+  },
+  {
+    "revision": "fd41b385fdef8a1634c9249f794bc6a3",
+    "url": "/static/media/ankara.fd41b385.svg"
+  },
+  {
+    "revision": "eb57f82002f5fc8a6af0793339f14a8a",
+    "url": "/static/media/istanbul.eb57f820.svg"
+  },
+  {
+    "revision": "80f870196328337ada75c5796fd830a1",
+    "url": "/static/media/izmir.80f87019.svg"
+  },
+  {
+    "revision": "340b58d3575c7e415b20c5febf86c324",
+    "url": "/static/media/canakkale.340b58d3.svg"
+  },
+  {
+    "revision": "231432cc53927a4d405dc41bd5227d4b",
+    "url": "/static/media/mugla.231432cc.svg"
+  },
+  {
+    "revision": "e28aeed941b68594b58ed6854ada0e2e",
+    "url": "/static/media/kayseri.e28aeed9.svg"
+  },
+  {
+    "revision": "1b074a7dd0a62d757465400514fec1a5",
+    "url": "/static/media/trabzon.1b074a7d.svg"
+  },
+  {
+    "revision": "7b1a503bbda6f9001e8db0e12ad41f70",
+    "url": "/static/media/hatay.7b1a503b.svg"
+  },
+  {
+    "revision": "5b993aea29bf7047b746418d96c69813",
+    "url": "/static/media/modelx.5b993aea.jpeg"
+  },
+  {
+    "revision": "abea189871e09d40ffdd3f5ae10cdd23",
+    "url": "/static/media/modelx_interior.abea1898.jpeg"
+  },
+  {
+    "revision": "68f6c417960af57a2431703cf97eb67c",
+    "url": "/static/media/model3_interior.68f6c417.jpeg"
+  },
+  {
+    "revision": "da612077d6c97dae5a453caea694c1ab",
+    "url": "/index.html"
+  }
+];
